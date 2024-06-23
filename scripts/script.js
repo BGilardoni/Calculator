707318508158da76f1c998cfaedf4213ec9c6468
Benjamin Gilardoni <benjamingilar@gmail.com>
2024-06-23 11:39:36 -0300

    Added basic memory

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,31 @@
+//Busca la pantalla de la calculadora por id
 const display = document.getElementById('display');
+//Guarda todos los botones
 const buttons = document.querySelectorAll('button');
+//Guarda el indicador de encendido y apagado por id
 const light = document.getElementById('light');
+//Para saber si esta prendida o apagada
 let estado = false;
+//Guarda la lista de la memoria por id
+let lista = document.getElementById("myList");
+//Para ir guardando los valores
+let memoria = [];
 
+//Agrega funcionalidad a los botones
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     if (estado){
         if (button.innerText === '=') {
         try {
             display.value = eval(display.value);
+            memoria.push(display.value);
+            lista.innerHTML ="";
+            memoria.forEach((item) => {
+                let li =
+                    document.createElement("li");
+                li.innerText = item;
+                lista.appendChild(li);
+            });
         } catch (error) {
             display.value = 'Error';
         }
@@ -35,4 +52,12 @@ buttons.forEach(button => {
         light.style.backgroundColor = 'green';
     }
   });
+});
+
+//Agrega elementos a la lista
+memoria.forEach((item) => {
+    let li =
+        document.createElement("li");
+    li.innerText = item;
+    lista.appendChild(li);
 });
